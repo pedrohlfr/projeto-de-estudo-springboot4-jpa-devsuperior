76686cb005464dc48c390fc10304150e5224bc51
Pedro Henrique <pedro2012lara@gmail.com>
2026-09-02 16:24:09 -0300

    class Category

---
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -0,0 +1,40 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.CourseApplication;
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	private final CourseApplication courseApplication;
+	@Autowired
+	private CategoryService service;
+
+	CategoryResource(CourseApplication courseApplication) {
+		this.courseApplication = courseApplication;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
